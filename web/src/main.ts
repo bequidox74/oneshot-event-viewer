66804ada477f8e478e3b5b4cb095977c7f13fe10
bequidox74 <aw4yland04@proton.me>
2026-08-24 16:16:38 +0300

    wip map tree

--- web/src/main.ts
+++ web/src/main.ts
@@ -5,7 +5,7 @@ for (const d of details) {
     sessionStorage.setItem(key, d.open.toString());
   });
   const value = sessionStorage.getItem(key);
-  if (value) d.open = (value == "true");
+  if (value) d.open = value == "true";
 }
 
 const games = ["os14", "os16", "wme", "pc", "frostide"];
@@ -14,33 +14,68 @@ for (const game of games) {
     `data/${game}/maps.json`,
   ).then((res) => res.json());
 
-  const ul = document.getElementById(game)!;
-  function makeLink(href: string, text: string, cls?: string): HTMLLIElement {
+  const mapList = document.getElementById(game)!;
+
+  const innerLists: Map<string, HTMLUListElement> = new Map();
+  const elems: Map<string, HTMLLIElement> = new Map();
+
+  function makeLink(
+    href: string,
+    text: string,
+    mapId: string,
+    cls?: string,
+  ): HTMLLIElement {
     const li = document.createElement("li");
+    const ul = document.createElement("ul");
     const a = document.createElement("a");
+
     a.href = href;
     a.textContent = text;
     if (cls) a.classList.add(cls);
+
     li.appendChild(a);
+    li.appendChild(ul);
+
+    if (mapId) innerLists.set(mapId, ul);
     return li;
   }
 
   const allMapsLink = makeLink(
     `dump?game=${game}&map=all`,
     "All Maps",
+    "all",
     "highlight",
   );
   allMapsLink.title = "Warning! This may lag your browser!";
-  ul.appendChild(allMapsLink);
-  ul.appendChild(
-    makeLink(`dump?game=${game}&map=common`, "Common Events", "highlight"),
+  mapList.appendChild(allMapsLink);
+  mapList.appendChild(
+    makeLink(
+      `dump?game=${game}&map=common`,
+      "Common Events",
+      "common",
+      "highlight",
+    ),
   );
+
+  // make all links
   for (const [id, name] of Object.entries(maps)) {
-    ul.appendChild(
+    elems.set(
+      id,
       makeLink(
         `dump?game=${game}&map=${id}`,
         `[${id}] ${name ?? "(unnamed map)"}`,
+        id,
       ),
     );
   }
+  
+  for (const [id, elem] of elems.entries()) {
+    const parent = innerLists.get(id) ?? mapList;
+    parent.appendChild(elem);
+  }
+  
+  // // prune empty lists
+  // for (const list of innerLists.values()) {
+  //   if (!list.hasChildNodes()) list.remove();
+  // }
 }
